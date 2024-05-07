e5f4d9af253b463b73438032ce2175cd83775f88
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-06 22:43:09 -0400

    feat: add margin-bottom to search bar component

--- src/components/searchBar/page.tsx
+++ src/components/searchBar/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SearchBarComponent = () => {
   return (
-    <section className='flex border-b border-gray-details pb-5 pt-8'>
+    <section className='flex border-b border-gray-details pb-5 pt-8 mb-7'>
       <input type="text" style={{backgroundColor: "transparent"}} className=" p-2 placeholder-gray-details focus:outline-none bg-transparent text-2xl text-gray-details" placeholder="Busque sus notas..." />
     </section>
   )
