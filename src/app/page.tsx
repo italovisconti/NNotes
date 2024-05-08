917ce14e1f1cfd10bb2d94a50f65ad51d1417100
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-07 22:06:27 -0400

    Refactor page layout to remove unnecessary border in main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+'use client'
 import { LogoSectionComponent } from "@/components/logoSection/page";
+import { ModalNoteComponent } from "@/components/modal/page";
 import { NoteFromList } from "@/components/noteFromList/page";
 import { SearchBarComponent } from "@/components/searchBar/page";
+import { Button, Modal } from 'antd';
+import { useState } from "react";
+
 interface Note {
   title: string;
   content: string;
@@ -11,11 +16,41 @@ const note: Note = {
 };
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const showModal = (event: any) => {
+    if(event.target.id === 'article' || event.target.id === 'article-text'){
+      setOpen(true);
+      console.log('Nota abierta');
+    }
+  };
+
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+  };
+
+  const handleCancel = (event: any) => {
+    if(event.target.id !== 'article-modal' ) {
+      setOpen(false);
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col py-14 px-32">
       <LogoSectionComponent />
       <SearchBarComponent />
-      <NoteFromList note={note}/>
+      <NoteFromList note={note} showModal={showModal}/>
+      {open && (
+        <div className="fixed top-0 left-0 w-full h-full z-50 flex flex-col justify-center items-center">
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-90" onClick={handleCancel}></div>
+          <ModalNoteComponent handleCancel={handleCancel} action={'Actualizar'}/>
+        </div>
+      )}
     </main>
   );
 }
