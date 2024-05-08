30a92bb4e91ec898216830290b1b3cc009ebe241
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-08 15:32:04 -0400

    Logic to show notes in the list of notes

--- src/components/noteList/page.tsx
+++ src/components/noteList/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NoteFromList } from '../noteFromList/page';
 import { Note, useAppContext } from '@/utils/AppContext';
 import { Spin } from 'antd';
@@ -9,18 +9,38 @@ export const NoteLIst = ({}) => {
     setAction,
     notes,
     loading,
+    notesSearched,
+    valueSearched,
   } = useAppContext();
+
+  const [ notesToRender, setNotesToRender ] = React.useState([]);
 
   const handleSetNote = (note: Note) => {
     localStorage.setItem('note', JSON.stringify(note));
     setAction('update');
   }
+  useEffect(() => {
+    if(notesSearched?.length > 0){
+      const notesAll =  notesSearched.map((note: Note) => {
+        return <NoteFromList key={note._id} note={note} showModal={showModal}  handleSetNote={handleSetNote}/>
+      })
+      setNotesToRender(notesAll)
+    }else{
+      if(valueSearched !== ''){
+        setNotesToRender([])
+      }else{
+        const notesALl = notes.map((note: Note) => {
+          return <NoteFromList key={note._id} note={note} showModal={showModal}  handleSetNote={handleSetNote}/>
+        })
+        setNotesToRender(notesALl)
+      }
+    }
+  }, [notesSearched, notes])
+
   return (
     <section className='flex flex-wrap gap-5 justify-center'>
       {loading
-        ?  notes.map((note: Note) => (
-            <NoteFromList key={note._id} note={note} showModal={showModal} handleSetNote={handleSetNote}/>
-          ))
+        ?  notesToRender.length !==0 ? notesToRender : <h1 className='text-3xl text-gray-details mt-48'>No se encontraron notas</h1>
         : <Spin size="large" className='scale-150 mt-48' />
       }
     </section>
--- src/components/searchBar/page.tsx
+++ src/components/searchBar/page.tsx
@@ -1,9 +1,37 @@
+import { Note, useAppContext } from '@/utils/AppContext';
 import React from 'react'
 
 export const SearchBarComponent = () => {
+  const {
+    notes,
+    notesSearched,
+    setNotesSearched,
+    setValueSearched
+  } = useAppContext();
+
+  const onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setValueSearched(searchTerm)
+    console.log("🚀 ~ onChangeSearch ~ searchTerm:", searchTerm)
+    if(searchTerm === ''){
+      setNotesSearched([])
+    }else{
+      if(notesSearched.length === 0){
+        setNotesSearched([...notes])
+      }else{
+        const searchedNotes = notes.filter((note:Note) => {
+          const searchInTitle = note.title.toLowerCase().includes(searchTerm);
+          const searchInContent = note.content.toLowerCase().includes(searchTerm);
+          return searchInTitle || searchInContent;
+        });
+        setNotesSearched(searchedNotes)
+      }
+    }
+  };
+
   return (
     <section className='flex border-b border-gray-details pb-5 pt-8 mb-7'>
-      <input type="text" style={{backgroundColor: "transparent"}} className=" p-2 placeholder-gray-details focus:outline-none bg-transparent text-2xl text-gray-details" placeholder="Busque sus notas..." />
+      <input type="text" style={{backgroundColor: "transparent"}} className=" p-2 placeholder-gray-details focus:outline-none bg-transparent text-2xl text-gray-details" placeholder="Busque sus notas..." onChange={onChangeSearch}/>
     </section>
   )
 }
