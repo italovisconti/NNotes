1f4d0e555bf5a89c63d21666b28ed4c9b53cee22
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-11 08:58:00 -0400

    chore: Update front-1.service to use /opt/scripts/script.sh for execution

--- src/scripts/script.sh
+++ src/scripts/script.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Entramos en el forlder app
-cd /opt/app/
-
-# Clonamos el repositorio
-git clone https://github.com/italovisconti/NNotes.git /opt/app/NNotes
-
-# Cambia al directorio del proyecto
-cd /NNotes
+cd /opt/app/NNotes
 
 # Instala las dependencias del proyecto
 npm install
