b51eff05374141e516b536aad4b9f221debb8cb6
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-11 10:07:16 -0400

    chore: Update script.sh to include logging statements for debugging

--- src/scripts/script.sh
+++ src/scripts/script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Entramos en el folder app
-echo "Entrando en el directorio /opt/app/NNotes ..."
+# Entramos en el forlder app
 cd /opt/app/NNotes
 
 # Instala las dependencias del proyecto
-echo "Instalando las dependencias del proyecto ..."
 npm install
 
 # Ejecuta el comando npm run build
-echo "Ejecutando el comando npm run build ..."
 npm run build
 
 # Ejecuta el comando npm start con el puerto 3001
-echo "Iniciando la aplicación con el comando npm start en el puerto 300X ..."
 npm start
