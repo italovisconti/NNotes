98dee6b464ef95571447f176f35dc06716c67100
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-05-12 08:10:43 -0400

    chore: Update script.sh to remove unnecessary permission change and add logging statements for debugging

--- src/scripts/script.sh
+++ src/scripts/script.sh
@@ -3,16 +3,13 @@
 # Entramos en el folder app
 cd /opt/app/NNotes
 
-if [ -d "/opt/app/NNotes/.next" ]; then
-  # Change permissions on .next directory if it exists
-  sudo chmod +x "/opt/app/NNotes/.next"
-fi
-
 # Instala las dependencias del proyecto with sudo
 sudo npm install
 
 # Ejecuta el comando npm run build
 npm run build
+
+sudo chmod +x "/opt/app/NNotes/.next"
 
 # Ejecuta el comando npm start con el puerto 3001
 npm start
